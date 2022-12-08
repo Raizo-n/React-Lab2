9d484486d8f8432640def867c0db174edb2bdf37
“Raizo-n” <sanpincha0826@gmail.com>
2022-12-08 01:15:29 -0800

    second commit

--- app.js
+++ app.js
@@ -25,4 +25,26 @@ const socialIcons = [
     }
 ]
 
-ReactDOM.render("test", document.getElementById('root'))
+function IconComponent({icon, title}) {
+    return (
+    <div className="button">
+        <div className="icon">
+            <i className='fa `fa-${icon}`'></i>
+        </div>
+        <span>{title}</span>
+    </div>
+    );
+  }
+
+const test = (
+    <div className={'wrapper'}>
+        <IconComponent icon={socialIcons[0].icon} title={socialIcons[0].title}/>
+        <IconComponent icon={socialIcons[1].icon} title={socialIcons[1].title}/>
+        <IconComponent icon={socialIcons[2].icon} title={socialIcons[2].title}/>
+        <IconComponent icon={socialIcons[3].icon} title={socialIcons[3].title}/>
+        <IconComponent icon={socialIcons[4].icon} title={socialIcons[4].title}/>
+        <IconComponent icon={socialIcons[5].icon} title={socialIcons[5].title}/>
+    </div>
+)
+
+ReactDOM.render(test, document.getElementById('root'))
